d338cf35fd7737e052496bb80fed8fcc2666fa20
Guilherme <guimiguel.machado@gmail.com>
2023-07-25 11:38:58 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -23,8 +24,12 @@ public class Program {
         }
         System.out.println("\n === Test 3: seller findAll ===");
         list = sellerDao.findAll();
-        for (Seller obj : list){
+        for (Seller obj : list) {
             System.out.println(obj);
-        
+        }
+        System.out.println("\n === Test 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = "+ newSeller.getId());
     }
 }
